9b23754c457f2467d37912ff058eef98be8e5911
Rheinard Korf <rheinard@thekorfs.com>
2023-04-28 11:52:12 +1000

    Fix netrw remaps.

--- .config/nvim/lua/rheinardkorf/remap.lua
+++ .config/nvim/lua/rheinardkorf/remap.lua
@@ -16,8 +16,9 @@ keymap.set({"n", "v", "o"}, "l", "u") -- Undo
 keymap.set({"n", "v", "o"}, "L", ":redo<CR>") -- Redo
 keymap.set({"n", "v", "o"}, "j", "o") -- Line Insert mode
 keymap.set({"n", "v", "o"}, "J", "O") -- 
--- keymap.set({"n", "v", "o"}, "j", "o") -- Undo
--- keymap.set({"n", "v", "o"}, "J", "O") -- Undo
+-- e, E --
+keymap.set({"n", "v", "o"},"m", "e")
+keymap.set({"n", "v", "o"},"M", "E")
 
 -- Window bindings
 keymap.set("n", "<leader>st", "<C-w>v") -- split window vertically
@@ -74,3 +75,36 @@ end)
 --
 -- -- keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 -- -- keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- ====== netrw remap =======
+local function netrw_mapping()
+  local bufmap = function(lhs, rhs)
+    local opts = {buffer = true, remap = true}
+    vim.keymap.set('n', lhs, rhs, opts)
+  end
+
+  -- close window
+  bufmap('<leader>dd', ':Lexplore<CR>')
+  bufmap('<leader>da', ':Lexplore<CR>')
+
+  -- Better navigation
+  -- bufmap('H', 'u')
+  -- bufmap('h', '-^')
+  -- bufmap('l', '<CR>')
+  -- bufmap('L', '<CR>:Lexplore<CR>')
+
+  -- Toggle dotfiles
+  bufmap('.', 'gh')
+  
+  -- Remap up and down
+  bufmap('i', '<Up>')
+  bufmap('e', '<Down>')
+end
+
+local user_cmds = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+vim.api.nvim_create_autocmd('filetype', {
+  pattern = 'netrw',
+  group = user_cmds,
+  desc = 'Keybindings for netrw',
+  callback = netrw_mapping
+})
