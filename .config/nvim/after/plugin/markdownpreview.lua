feb7357b90b38c2759291bf548f648853eb331e3
Rheinard Korf <rheinard@thekorfs.com>
2023-06-26 13:40:59 +1000

    Sync nvim config.

---
+++ .config/nvim/after/plugin/markdownpreview.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>mp", vim.cmd.MarkdownPreview);
+vim.keymap.set("n", "<leader>ms", vim.cmd.MarkdownPreviewStop);
+vim.keymap.set("n", "<leader>mt", vim.cmd.MarkdownPreviewToggle);
