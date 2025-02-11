ae62d5861d21148209ac5537d66dd1248a20bcbd
Rheinard Korf <rheinard@thekorfs.com>
2025-02-11 12:05:26 +1100

    Fix broken cursor config.

--- .config/zsh/configs/post/99-cursor.zsh
+++ .config/zsh/configs/post/99-cursor.zsh
@@ -1,9 +1,11 @@
 export CURSOR_HOME="$HOME/.config/cursor"
 
-if [ -d "$HOME/.cursor" ]; then
-    rm -rf "$HOME/.cursor"
-fi
+# We don't want to use the default cursor dirs location.
+# if [ -d "$HOME/.cursor" ]; then
+   rm -rf "$HOME/.cursor"
+#fi
 
-if [ ! -d "$CURSOR_HOME" ]; then
-    ln -s ~/.config/cursor ~/.cursor
+# Symlink $HOME/.cursor to $CURSOR_HOME if the symlink doesn't exist.
+if [ ! -L "$HOME/.cursor" ]; then
+   ln -s "$CURSOR_HOME" "$HOME/.cursor"
 fi
