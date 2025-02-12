cd7a54bac0a4e059be08fb60fa50473aafd5c6e5
Rheinard Korf <rheinard@thekorfs.com>
2025-02-13 09:32:42 +1100

    Disable custom ollama nvim plugin.

--- .config/nvim/lua/rheinard/plugins/ollama.lua
+++ .config/nvim/lua/rheinard/plugins/ollama.lua
@@ -1,13 +1,15 @@
-return {
-    dir = "/Users/rheinardkorf/Development/nvim/nvim-ollama",
-    dependencies = {
-        "MunifTanjim/nui.nvim"
-    },
-    config = function()
-        require("ollama").setup({
-            autostart = false,
-            model = "mistral",
-        })
-    end
-}
+return {}
+-- Disabling until I feel like picking it up again.
+-- return {
+--     dir = "/Users/rheinardkorf/Development/nvim/nvim-ollama",
+--     dependencies = {
+--         "MunifTanjim/nui.nvim"
+--     },
+--     config = function()
+--         require("ollama").setup({
+--             autostart = false,
+--             model = "mistral",
+--         })
+--     end
+-- }
 
